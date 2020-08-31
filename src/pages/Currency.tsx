00db6eeb58d59d0a869482d5f7c6ee7b7ac6914e
dominikvrbic <dominikvrbic01@gmail.com>
2020-08-31 16:34:09 +0200

    mobile fix

--- src/pages/Currency.tsx
+++ src/pages/Currency.tsx
@@ -1,52 +1,98 @@
 import React from "react";
 import { useParams } from "react-router";
 import { css } from "emotion";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { coinStore, coin } from "../stores/coinStore";
+import { coinStore } from "../stores/coinStore";
 
 interface Props {}
+
 const card = css({
-  padding: "15px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  padding: "15px 0px",
   boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
   transition: "0.3s",
   width: "50%",
   "&:hover": {
-    boxShadow: " 0 8px 16px 0 rgba(0,0,0,0.2)",
+    boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)",
   },
-  "@media(max-width: 1200px) ": {
-    width: "100%",
+  "@media(max-width: 1200px)": {
+    width: "95%",
+    padding: "10px 3px",
   },
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
 });
+
 const topPart = css({
   display: "flex",
   borderBottom: "1px solid #ddd",
-
-  paddingBottom: "10px",
+  padding: "10px 0px",
 });
+
 const blueText = css({
   paddingRight: "25px",
   fontSize: "30px",
   fontWeight: "bolder",
   color: "rgb(0, 0, 128)",
+  "@media(max-width: 1200px)": {
+    fontSize: "14px",
+  },
+  "@media(max-width: 800px)": {
+    fontSize: "10px",
+  },
 });
+
 const grayText = css({
   paddingRight: "30px",
   fontSize: "20px",
   fontWeight: "bold",
   color: "gray",
+  "@media(max-width: 1200px)": {
+    fontSize: "10px",
+  },
+  "@media(max-width: 800px)": {
+    fontSize: "6px",
+  },
 });
+
 const blackText = css({
   fontSize: "25px",
   paddingRight: "30px",
   fontWeight: "bolder",
+  "@media(max-width: 1200px)": {
+    fontSize: "12px",
+  },
+  "@media(max-width: 800px)": {
+    fontSize: "8px",
+  },
 });
+
 const bootomPart = css({
-  paddingTop: "15px",
+  padding: "10px 0px",
   display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
 });
+
+const assetNameStyle = css({
+  fontSize: "24px",
+  fontWeight: "bold",
+  "@media(max-width: 1200px)": {
+    fontSize: "12px",
+  },
+  "@media(max-width: 800px)": {
+    fontSize: "8px",
+  },
+});
+
+const bottomPiece = css({
+  "@media(max-width: 1200px)": {
+    maxWidth: "100px",
+  },
+  "@media(max-width: 800px)": {
+    maxWidth: "60px",
+  },
+});
+
 const Currency = (props: Props) => {
   //@ts-ignore
   let { name } = useParams();
@@ -56,10 +102,17 @@ const Currency = (props: Props) => {
     fontSize: "25px",
     paddingRight: "30px",
     fontWeight: "bolder",
+    "@media(max-width: 1200px)": {
+      fontSize: "12px",
+    },
+    "@media(max-width: 800px)": {
+      fontSize: "8px",
+    },
   });
+
   return (
     <div className={card}>
-      <div>{currency.assetName}</div>
+      <div className={assetNameStyle}>{currency.assetName}</div>
       <div className={topPart}>
         <div>
           <div className={blueText}>{currency.marketSymbol}</div>
@@ -74,19 +127,19 @@ const Currency = (props: Props) => {
       </div>
 
       <div className={bootomPart}>
-        <div>
+        <div className={bottomPiece}>
           <div className={blackText}>
             ${Number(currency.lastPrice).toFixed(2)}
           </div>
           <div className={grayText}>Last Price </div>
         </div>
-        <div>
+        <div className={bottomPiece}>
           <div className={change}>
             {Number(currency.percentChange).toFixed(2)}
           </div>
           <div className={grayText}>24h Change </div>
         </div>
-        <div>
+        <div className={bottomPiece}>
           <div className={blackText}>
             {currency.lowPrice}
             {currency.marketSymbol.substr(
@@ -96,7 +149,7 @@ const Currency = (props: Props) => {
           </div>
           <div className={grayText}>24h Low </div>
         </div>
-        <div>
+        <div className={bottomPiece}>
           <div className={blackText}>
             {currency.highPrice}
             {currency.marketSymbol.substr(
@@ -110,4 +163,5 @@ const Currency = (props: Props) => {
     </div>
   );
 };
+
 export default Currency;
